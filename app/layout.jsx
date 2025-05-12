5f710ffbcd150be38e03725d5e1a4491b12b930c
Arunav Singh <arunavsingh999@gmail.com>
2025-05-12 11:02:29 +0530

    restart

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,7 @@
 import "./globals.css";
 
+import Header from "@/components/Header";
+
 export const metadata = {
   title: "Reddit_Clone",
   description: "This is a reddit_clone app.",
@@ -8,7 +10,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
