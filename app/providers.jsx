c336b1dbe787bda9fc2b0a6287563dc43a81a7b1
Arunav Singh <arunavsingh999@gmail.com>
2025-05-17 14:00:37 +0530

    removed apollo client

--- app/providers.jsx
+++ app/providers.jsx
@@ -1,16 +1,11 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
-import { ApolloProvider } from "@apollo/client";
-
-import client from "@/apollo-client";
 
 export default function Providers({ children }) {
   return (
     <>
-      <ApolloProvider client={client}>
-        <SessionProvider>{children}</SessionProvider>
-      </ApolloProvider>
+      <SessionProvider>{children}</SessionProvider>
     </>
   );
 }
